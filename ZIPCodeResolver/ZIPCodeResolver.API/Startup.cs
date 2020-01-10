1dd3eb8c78970dbb7595a30f8cff0291b7d64772
barabas <vitalii.shulenin@gmail.com>
2020-01-10 16:32:07 +0100

    removed extra libraries

--- ZIPCodeResolver/ZIPCodeResolver.API/Startup.cs
+++ ZIPCodeResolver/ZIPCodeResolver.API/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Mime;
 using System.Web.Http;
 
 namespace ZIPCodeResolver.API
@@ -9,6 +11,17 @@ namespace ZIPCodeResolver.API
     {
         public static void Register(HttpConfiguration config)
         {
+            var jsonTypeMapping = new RequestHeaderMapping(
+                "Accept",
+                MediaTypeNames.Text.Html,
+                StringComparison.InvariantCultureIgnoreCase,
+                true,
+                "application/json");
+            var jsonFormatter = config.Formatters.JsonFormatter;
+
+            jsonFormatter.MediaTypeMappings.Add(jsonTypeMapping);
+
+
             // Web API routes
             config.MapHttpAttributeRoutes();
         }
